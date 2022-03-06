652247ccf3c6fa53ec2a9309fa9f362637942846
tobialbert <daadatobi@gmail.com>
2022-03-06 14:43:31 +0100

    Feat: Implement use cases.

--- app/src/main/java/com/tobidaada/community/features/community/domain/usecase/GetMembers.kt
+++ app/src/main/java/com/tobidaada/community/features/community/domain/usecase/GetMembers.kt
@@ -1,11 +1,20 @@
 package com.tobidaada.community.features.community.domain.usecase
 
+import com.tobidaada.community.di.IOCoroutineDispatcher
+import com.tobidaada.community.features.community.ResultWrapper
 import com.tobidaada.community.features.community.domain.CommunityRepository
+import com.tobidaada.community.features.community.domain.entities.User
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetMembers @Inject constructor(
-    private val communityRepository: CommunityRepository
+    private val communityRepository: CommunityRepository,
+    @IOCoroutineDispatcher private val ioDispatcher: CoroutineDispatcher
 ) {
 
+    suspend operator fun invoke(page: Int): Flow<ResultWrapper<List<User>>> =
+        withContext(ioDispatcher) { communityRepository.getMembers(page) }
 }
 
--- app/src/main/java/com/tobidaada/community/features/community/domain/usecase/UpdateMemberLike.kt
+++ app/src/main/java/com/tobidaada/community/features/community/domain/usecase/UpdateMemberLike.kt
@@ -1,4 +1,19 @@
 package com.tobidaada.community.features.community.domain.usecase
 
-class UpdateMemberLike{
+import com.tobidaada.community.di.IOCoroutineDispatcher
+import com.tobidaada.community.features.community.domain.CommunityRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class UpdateMemberLike @Inject constructor(
+    private val communityRepository: CommunityRepository,
+    @IOCoroutineDispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+
+    suspend operator fun invoke(params: Params) = withContext(ioDispatcher) {
+        communityRepository.updateMemberLike(userId = params.userId, isLiked = params.isLiked)
+    }
+
+    data class Params(val userId: Int, val isLiked: Boolean)
 }
