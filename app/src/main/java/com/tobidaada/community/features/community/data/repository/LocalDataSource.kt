ad3a1b9a40547e9f1da76f8e5687bca4720e4350
tobialbert <daadatobi@gmail.com>
2022-03-07 10:00:06 +0100

    Refactor: Remove unused classes

--- app/src/main/java/com/tobidaada/community/features/community/data/repository/LocalDataSource.kt
+++ app/src/main/java/com/tobidaada/community/features/community/data/repository/LocalDataSource.kt
@@ -1,11 +1,8 @@
 package com.tobidaada.community.features.community.data.repository
 
 import androidx.paging.PagingSource
-import androidx.paging.PagingState
 import com.tobidaada.community.features.community.data.datasource.local.models.UserWithLike
 import com.tobidaada.community.features.community.data.datasource.shared.UserDataSource
-import com.tobidaada.community.features.community.data.models.UserData
-import kotlinx.coroutines.flow.Flow
 
 interface LocalDataSource {
 
